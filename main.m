58c107a337092ad932b5873274a95625c90aa7a0
WenbinWang <147360189+WenbinWang024@users.noreply.github.com>
2024-07-18 21:17:17 +0800

    update main

--- main.m
+++ main.m
@@ -1,31 +1,43 @@
 clear,clc
 rng(0);
 
-dim = 100;
-Sigma_star = Generated_Banded_Matrix(100, 10);
+dim = 50;
+Sigma_star = Generated_Toeplitz_Matrix(dim);
+% Generated_Banded_Matrix(50, 5);
 % full(sprandsym(50, 0.032, 0.2, 1));
 
 error_para = 1e-1;
 error_list_non = [];
+error_list_non_norm2 = [];
 error_list_L1 = [];
+error_list_L1_norm2 = [];
 error_list_hybrid = [];
-m_list = [80, 120, 180, 200, 240, 280, 320, 360, 400, 440, 500, 550, 600];
-for idx_list = 1:13
+FPR_non = [];
+FPR_L1 = [];
+TPR_non = [];
+TPR_L1 = [];
+m_list = [100, 500, 1000, 1500];
+for idx_list = 1:4
     m = m_list(idx_list);
     para.a_Penalty = 2;
-    para.inter_max_out = 1e3;
+    para.inter_max_out = 10;
     para.inter_max_in = 1e5;
     para.tol_opt = 1e-6;
     para.max_lineiter = 1e6;
-    para.EPS = 2.22E-16;
+    para.EPS = 2.22E-6;
     para.Sigma_star = Sigma_star;
     
     subvar = size(Sigma_star, 2);
     A = randn(subvar, m); 
+
+    num_samples = 50;
+    mu = zeros(1, dim);
+    samples = mvnrnd(mu, Sigma_star, num_samples);
+    Sigma_sample = cov(samples);
     
     Y = [];
     for i = 1:m
-        Y = [Y; A(:, i)' * Sigma_star * A(:, i) + error_para * (2 * rand() - 1)];
+        Y = [Y; A(:, i)' * Sigma_sample * A(:, i) + error_para * (2 * rand() - 1)];
     end
     % + error_para * (2 * rand() - 1)
     
@@ -37,8 +49,8 @@ for idx_list = 1:13
     minError = inf;
 
     % The possible value of the hyperparameter
-    tauValues = logspace(-10, -1, 10);
-    lambdaValues = 0.1:0.1:1;
+    tauValues = logspace(-8, -4, 5);
+    lambdaValues = 0.1:0.1:0.5;
 
     % 对每一对超参数进行交叉验证
     for tau = tauValues
@@ -144,10 +156,48 @@ for idx_list = 1:13
     [Sigma_non, values_non, para] = MCRA_PNM(Sigma, para);
     non_zero_count = nnz(Sigma_star);
     error_list_non = [error_list_non; non_zero_count, norm(Sigma_non - Sigma_star, 'fro'), norm(Sigma_star, 'fro'), norm(Sigma_non - Sigma_star, 'fro') / norm(Sigma_star, 'fro')];
+    error_list_non_norm2 = [error_list_non_norm2; non_zero_count, norm(Sigma_non - Sigma_star), norm(Sigma_star), norm(Sigma_non - Sigma_star) / norm(Sigma_star)];
+    FPR_num = 0;
+    for idx1 = 1:dim
+        for idx2 = 1:dim
+            if Sigma_non(idx1, idx2) ~= 0 && Sigma_star(idx1, idx2) == 0
+                FPR_num = FPR_num + 1;
+            end
+        end
+    end
+    FPR_non = [FPR_non; non_zero_count, FPR_num / dim * dim - non_zero_count];
+    TPR_num = 0;
+    for idx1 = 1:dim
+        for idx2 = 1:dim
+            if Sigma_non(idx1, idx2) ~= 0 && Sigma_star(idx1, idx2) ~= 0
+                TPR_num = TPR_num + 1;
+            end
+        end
+    end
+    TPR_non = [TPR_non; non_zero_count, TPR_num / non_zero_count];
     [Sigma_L1, values_L1, para] = MCRA_PNM_L1(Sigma, para);
     error_list_L1 = [error_list_L1; non_zero_count, norm(Sigma_L1 - Sigma_star, 'fro'), norm(Sigma_star, 'fro'), norm(Sigma_L1 - Sigma_star, 'fro') / norm(Sigma_star, 'fro')];
-    [Sigma_hybrid, values_hybrid, para] = MCRA_PNM_Hybrid(Sigma, para);
-    error_list_hybrid = [error_list_hybrid; non_zero_count, norm(Sigma_hybrid - Sigma_star, 'fro'), norm(Sigma_star, 'fro'), norm(Sigma_hybrid - Sigma_star, 'fro') / norm(Sigma_star, 'fro')];
+    error_list_L1_norm2 = [error_list_L1_norm2; non_zero_count, norm(Sigma_L1 - Sigma_star), norm(Sigma_star), norm(Sigma_L1 - Sigma_star) / norm(Sigma_star)];
+    FPR_num = 0;
+    for idx1 = 1:dim
+        for idx2 = 1:dim
+            if Sigma_L1(idx1, idx2) ~= 0 && Sigma_star(idx1, idx2) == 0
+                FPR_num = FPR_num + 1;
+            end
+        end
+    end
+    FPR_L1 = [FPR_L1; non_zero_count, FPR_num / dim * dim - non_zero_count];
+    TPR_num = 0;
+    for idx1 = 1:dim
+        for idx2 = 1:dim
+            if Sigma_L1(idx1, idx2) ~= 0 && Sigma_star(idx1, idx2) ~= 0
+                TPR_num = TPR_num + 1;
+            end
+        end
+    end
+    TPR_L1 = [TPR_L1; non_zero_count, TPR_num / non_zero_count];
+    % [Sigma_hybrid, values_hybrid, para] = MCRA_PNM_Hybrid(Sigma, para);
+    % error_list_hybrid = [error_list_hybrid; non_zero_count, norm(Sigma_hybrid - Sigma_star, 'fro'), norm(Sigma_star, 'fro'), norm(Sigma_hybrid - Sigma_star, 'fro') / norm(Sigma_star, 'fro')];
 end
 
 save("data.mat")
